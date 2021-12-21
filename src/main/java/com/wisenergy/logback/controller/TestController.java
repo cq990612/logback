47ae8a8ed597c4a03f5d40b15a584fb54dbed466
chenqi <1799796883@qq.com>
2021-12-21 16:51:48 +0800

    Initial commit

---
+++ src/main/java/com/wisenergy/logback/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.wisenergy.logback.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping("test")
+public class TestController {
+
+
+    @GetMapping("/sendMsg/{message}")
+    public String sendMsg(@PathVariable("message") String message) {
+        log.info("客户端发送消息:{}", message);
+        return message;
+    }
+}
